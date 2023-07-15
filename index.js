bea84370543096a580e08a2d43d16222017db87f
koheylux <koheylux.0224f@pets-mail.com>
2023-07-16 03:22:43 +0900

    complete TODOlist

--- index.js
+++ index.js
@@ -1,43 +1,71 @@
 const onClickAdd = () => {
-  //テキストボックスの値を取得　&　初期化
+  //テキストボックスの値を取得
   const inputText = document.getElementById("add-text").value;
+  const p = document.createElement("p");      //pタグ
+  p.innerText = inputText;
+
+  //テキストボックス内を初期化
   document.getElementById("add-text").value = "";
 
-  //liタグの生成
-  const li = document.createElement("li");
+  addIncompleteList(p);
+};
 
 
-  //divタグの中身生成
-  const div = document.createElement("div");
-  div.className = "list-row";
+//未完了リストに追加する関数
+const addIncompleteList = (p) => {
+  const li = document.createElement("li");    //liタグ
+  const div = document.createElement("div");  //divタグ
+  const compliteButton = document.createElement("button");//完了buttonタグ
+  const deleteButton = document.createElement("button");  //削除buttonタグ
 
-  const p = document.createElement("p"); //pタグの生成
-  const compliteButton = document.createElement("button"); //完了buttonタグの生成
-  const deleteButton = document.createElement("button"); //削除buttonタグの生成
+  //divタグにクラス名付与
+  div.className = "list_row";
 
-  p.innerText = inputText; //pに入力文字を格納
   compliteButton.innerText = "完了";
   deleteButton.innerText = "削除";
 
+  //完了ボタンの処理
   compliteButton.addEventListener("click", () => {
-    
+    deleteButton.parentNode.parentNode.remove();
+    addCompleteList(p);
   });
 
+  //削除ボタンの処理
   deleteButton.addEventListener("click", () => {
-    const deleteTarget = deleteButton.parentNode.parentNode;
-    document.getElementById("incomplete-list").removeChild(deleteTarget);
+    deleteButton.parentNode.parentNode.remove();
   });
 
-  div.appendChild(p); //pをdivにアペンド
-  div.appendChild(compliteButton); //完了ボタンをdivにアペンド
-  div.appendChild(deleteButton); //削除ボタンをdivにアペンド
+  div.appendChild(p);               //pをdivにアペンド
+  div.appendChild(compliteButton);  //完了ボタンをdivにアペンド
+  div.appendChild(deleteButton);    //削除ボタンをdivにアペンド
+  li.appendChild(div);              //divをliにアペンド
 
-  //liタグの子要素にdivを設定
-  li.appendChild(div);
-
-  //未完了リストに追加
-  document.getElementById("incomplete-list").appendChild(li);
+  document.getElementById("incomplete_list").appendChild(li); //未完了TODOに追加
 };
 
+
+//完了リストに追加する関数
+const addCompleteList = (p) => {
+  const li = document.createElement("li");    //liタグ
+  const div = document.createElement("div");  //divタグ
+  const backButton = document.createElement("button");  //戻すbuttonタグ
+
+  //divタグにクラス名付与
+  div.className = "list_row";
+
+  backButton.innerText = "戻す";
+
+  //戻すボタンの処理
+  backButton.addEventListener("click", () => {
+    backButton.parentNode.parentNode.remove();
+    addIncompleteList(p);
+  })
+
+  div.appendChild(p);               //pをdivにアペンド
+  div.appendChild(backButton);      //戻すボタンをdivにアペンド
+  li.appendChild(div);              //divをliにアペンド
+
+  document.getElementById("complete_list").appendChild(li); //完了TODOに追加
+};
 
 document.getElementById("add-button").addEventListener("click", onClickAdd);
